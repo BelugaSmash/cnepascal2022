c0992c143ad43dfeb4fd9336a263a8bff1f41d43
htjung09 <htjung09@naver.com>
2022-06-18 14:22:03 +0900

    hh

--- flappybird.py
+++ flappybird.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode((1280, 720))
 
 gy = 0
 x, y, w, h = 100, 720 / 2, 50, 50
-pipex, pipey, pipew, pipeh = 1280, random.randint(720 / 2 - 200, 720 / 2 + 200), 100, 200
+pipex, pipey, pipew, pipeh = 1280, random.randint(720/2, 720/2 + 100), 100, 550
 
 def collide(x, y, w, h, x_, y_, w_, h_):
     return x < x_ + w_ and y < y_ + h_ and x + w > x_ and y + h > y_
@@ -29,12 +29,14 @@ while 1:
     gy -= 0.5
     y -= gy
     if pipex <= 0 - pipew:
-        pipex = 1300
-    pygame.draw.rect(screen, (0, 255, 0), [pipex - pipew / 2, pipeh - 720 / 2, pipew, pipeh])
-    pygame.draw.rect(screen, (0, 255, 0), [pipex - pipew / 2, pipeh + 720 / 2, pipew, pipeh])
+        pipex = 1280 + pipew
+        pipey = random.randint(720/2, 720/2 + 300)
+    
+    pygame.draw.rect(screen, (0, 255, 0), [pipex - pipew / 2, pipey - 720 / 2 - 350, pipew, pipeh])
+    pygame.draw.rect(screen, (0, 255, 0), [pipex - pipew / 2, pipey + 50, pipew, pipeh])
     pygame.draw.rect(screen, (0, 0, 255), [x - w / 2, y - h / 2, w, h])
-    if collide(x - w / 2, y - h / 2, w, h, pipex - pipew / 2, pipeh - 720 / 2, pipew, pipeh) or\
-        collide(x - w / 2, y - h / 2, w, h, pipex - pipew / 2, pipeh + 720 / 2, pipew, pipeh) or\
+    if collide(x - w / 2, y - h / 2, w, h, pipex - pipew / 2, pipey - 720 / 2 - 350, pipew, pipeh) or\
+        collide(x - w / 2, y - h / 2, w, h, pipex - pipew / 2, pipey + 50, pipew, pipeh) or\
         y < 0 or y > 720:
         font1 = pygame.font.SysFont(None,100)
         txt = font1.render('Game Over!',True,(0, 0, 0))
