4ac3a391f2638c39a45d7ac414cb1bf6278347ef
htjung09 <htjung09@naver.com>
2022-06-18 14:58:05 +0900

    hh

--- stock.py
+++ stock.py
@@ -8,6 +8,7 @@ sample = 10000
 stock_num_save = 0
 global flag_first
 flag_first=0
+w, h = 128, 720/2
 while 1:
     if flag_first==1:
         break
@@ -15,14 +16,23 @@ while 1:
         for i in range(0, 10):
             stock_num_save = random.randint(-100, 100)
             #time.sleep(1)
-            stock_num.append(sample - stock_num_save)
+            stock_num.append(sample + stock_num_save)
             if i == 9:
                 flag_first=1
                 break
         print(stock_num)
+
 while 1:
     screen = pygame.display.set_mode((1280,720))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
+
     screen.fill((255,255,255))
+    for i in range(0, 10):
+        if stock_num[i] < 10000:
+            pygame.draw.rect(screen, (0, 0, 255), [w * i, 720/2, w, (10000 - stock_num[i]) / 100 * h])
+        else:
+            pygame.draw.rect(screen, (255, 0, 0), [w * i, 720/2 - (stock_num[i] - 10000) / 100 * h, w, (stock_num[i] - 10000) / 100 * h])
+
+    pygame.display.update()
