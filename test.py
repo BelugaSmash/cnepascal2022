e0b7b815658c42c176b7b29e7d33283d255d8b05
blackboxleewon <107734291+blackboxleewon@users.noreply.github.com>
2022-07-15 20:15:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello world")
+#인공지능을 적이아닌 도우미로 만들기(주가 상승하락보여주기 등등)
+
+import random
+
+#부족하지만 등락을 정하는 코드
+updown = list(range(10))
+for i in range(1,11):
+    num = random.randint(1,6)
+    if num >= 1 and num < 4:
+        updown[i] = 1
+    if num >= 4 and num < 7:
+        updown[i] = 2 
+    print(updown[i])
+#후에는 그래프 그리기를 넣고 인공지능을 탑재 
